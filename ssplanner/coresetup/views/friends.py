12058a687a0759e3df67cb0900f1d33f922a0f9a
Bharathi Raja <bharathiraja@Bharathis-MacBook-Pro.local>
2020-05-11 20:03:00 +0530

    Changes to Friends

--- ssplanner/coresetup/views/friends.py
+++ ssplanner/coresetup/views/friends.py
@@ -23,30 +23,44 @@ class FriendView(APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request):
-        friends = Friend.objects.filter(pk=request.user.id)
+        print(request.user.id)
+        friends = Friend.objects.filter(current_user=request.user.id)
         friendsserializer = FriendsSerializer(friends, many=True)
         return Response(friendsserializer.data, status.HTTP_200_OK)  
 
     def post(self, request):
+        # TODO create user and add as friend(Split Directly)
+        list_users = request.data
+        print(list_users['user_list'])
         current_user = request.user.id
-        user = request.data['user']
-        # for user in users:
         current_contact = Contact.objects.filter(pk=current_user).first()
-        create_friend = Friend.make_friend(
-            current_user=current_contact,
-            new_friend=user
-        )
-        if create_friend:
-            friends = Friend.objects.filter(users=user)
-            friendsserializer = FriendsSerializer(friends)
-            return Response(friendsserializer.data, status.HTTP_201_CREATED)
-        # TODO implement else part if not created
+        for user in list_users['user_list']:
+            contact, created = Contact.objects.get_or_create(
+                user_name=user['user_name']
+            )            
+            if not created:
+                contact.is_active = False
+                contact.save()                
+                create_friend = Friend.make_friend(
+                    current_user=current_contact,
+                    new_friend=contact
+                )
+            else:
+                create_friend = Friend.make_friend(
+                    current_user=current_contact,
+                    new_friend=contact
+                )
+            
+        friends = Friend.objects.filter(users=current_user)
+        friendsserializer = FriendsSerializer(friends)
+        return Response(friendsserializer.data, status.HTTP_201_CREATED)
+        
 
 
 class FriendDetailView(APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request, pk, format=None):
-        friends = Friend.objects.filter(pk=pk)
+        friends = Friend.objects.filter(current_user=request.user.id)
         friendsserializer = FriendsSerializer(friends, many=True)
         return Response(friendsserializer.data, status.HTTP_200_OK)  
